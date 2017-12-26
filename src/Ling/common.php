55a8d346061a637e0d1140081230ec47c27e8317
fri13th <tenderain@gmail.com>
2017-12-26 16:54:39 +0900

    fix for php 5.3, but some test codes can run in php 5.4

--- src/Ling/common.php
+++ src/Ling/common.php
@@ -17,44 +17,51 @@ if (!function_exists('config')) {
             return null;
         }
 
-        return (isset($CONFIG[$key])) ? $CONFIG[$key] : $default;
+        return isset($CONFIG[$key]) ? $CONFIG[$key] : $default;
     }
 }
 
 if (!function_exists('hook')) {
-    function hook($hook_id, $args = null) {
+
+    function hook($hook_id, $args = array()) {
+        /** @var array $hooks */
         $hooks = config($hook_id);
-        if ($args == null) $args = array();
         if ($hooks && is_array($args)) {
             foreach ($hooks as $hook) {
                 call_user_func_array($hook, $args);
                 //$hook(...$args); // PHP 5.6+
             } 
         } else if (is_callable($args)){ // save to hook
-            if (!$hooks) $hooks = [];
-            array_push($hooks, $args);
-            config([$hook_id => $hooks]);
+            if (!$hooks) {
+                $hooks = array();
+            }
+            $hooks[] = $args;
+            config(array($hook_id => $hooks));
         }
     }
 }
 
 if (!function_exists('session')) {
     function session($key, $val = null) {
-        $session = $_SESSION["LING_SESSION"];
-        if (isNull($val)) {
-            if ($session && isset($session[$key])) return $session[$key];
+        $session = $_SESSION['LING_SESSION'];
+        if (null === $val) {
+            if ($session && isset($session[$key])) {
+                return $session[$key];
+            }
         } else {
-            if (!$session) $session = array();
+            if (!$session) {
+                $session = array();
+            }
             $session[$key] = $val;
-            $_SESSION["LING_SESSION"] = $session;
+            $_SESSION['LING_SESSION'] = $session;
         } 
         return null;
     }
 }
 
 if (!function_exists('cookie')) {
-    // we don't provide setcookie, because there's built-in setcookie
+    // we don't provide set_cookie, because there's built-in function setcookie
     function cookie($key) {
-        return isset($_COOKIE[$key]) ? $_COOKIE[$key] : "";
+        return isset($_COOKIE[$key]) ? $_COOKIE[$key] : '';
     }
 }
--- tests/Ling/commonTest.php
+++ tests/Ling/commonTest.php
@@ -2,23 +2,21 @@
 namespace Ling;
 
 use PHPUnit\Framework\TestCase;
-use function Ling\config as config;
-use function Ling\hook as hook;
 
 class commonTest extends TestCase
 {
     public $hook_result;
 
     public function testConfig() {
-        config(["123" => "456"]);
-        $this->assertTrue(config("123") === "456");
+        config(['123' => '456']);
+        $this->assertSame(config('123'), '456');
     }
     
     public function testHook() {
-        hook("hook.hook_id", function ($hello, $world) {
-            $this->hook_result =  $hello . ", " . $world;
+        hook('hook.hook_id', function ($hello, $world) {
+            $this->hook_result =  $hello . ', ' . $world;
         });
-        hook("hook.hook_id", ["Hello", "world!"]);
-        $this->assertTrue($this->hook_result == "Hello, world!");
+        hook('hook.hook_id', ['Hello', 'world!']);
+        $this->assertSame($this->hook_result, 'Hello, world!');
     }
 }
