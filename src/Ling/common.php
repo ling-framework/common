faddad84725598bdfe4d9dcccd621aa01a41924d
fri13th <tenderain@gmail.com>
2018-01-16 16:51:35 +0900

    fix structure and add env feature

--- src/Ling/common.php
+++ src/Ling/common.php
@@ -1,67 +1,42 @@
 <?php
 
 namespace Ling;
+use function Ling\startsWith;
 
-if (!function_exists('config')) {
+$GLOBALS['LING_CONFIG'] = array();
 
-    $GLOBALS['LING_CONFIG'] = array();
+function config($key, $default = null) {
 
-    function config($key, $default = null) {
-        $CONFIG = $GLOBALS['LING_CONFIG'];
+    $CONFIG = $GLOBALS['LING_CONFIG'];
 
-        if (is_array($key)) {
-            foreach($key as $k => $v) {
-                $CONFIG[$k] = $v;
-            }
-            $GLOBALS['LING_CONFIG'] = $CONFIG;
-            return null;
+    if (is_array($key)) {
+        foreach($key as $k => $v) {
+            $CONFIG[$k] = $v;
         }
-
-        return isset($CONFIG[$key]) ? $CONFIG[$key] : $default;
-    }
-}
-
-if (!function_exists('hook')) {
-
-    function hook($hook_id, $args = array()) {
-        /** @var array $hooks */
-        $hooks = config($hook_id);
-        if ($hooks && is_array($args)) {
-            foreach ($hooks as $hook) {
-                call_user_func_array($hook, $args);
-                //$hook(...$args); // PHP 5.6+
-            } 
-        } else if (is_callable($args)){ // save to hook
-            if (!$hooks) {
-                $hooks = array();
-            }
-            $hooks[] = $args;
-            config(array($hook_id => $hooks));
-        }
-    }
-}
-
-if (!function_exists('session')) {
-    function session($key, $val = null) {
-        $session = $_SESSION['LING_SESSION'];
-        if (null === $val) {
-            if ($session && isset($session[$key])) {
-                return $session[$key];
-            }
-        } else {
-            if (!$session) {
-                $session = array();
-            }
-            $session[$key] = $val;
-            $_SESSION['LING_SESSION'] = $session;
-        } 
+        $GLOBALS['LING_CONFIG'] = $CONFIG;
         return null;
     }
+
+    if (startsWith($key, 'env.')) { // if some config starts with env, get variable from env
+        return getenv(substr($key, 4));
+    }
+    return isset($CONFIG[$key]) ? $CONFIG[$key] : $default;
 }
 
-if (!function_exists('cookie')) {
-    // we don't provide set_cookie, because there's built-in function setcookie
-    function cookie($key) {
-        return isset($_COOKIE[$key]) ? $_COOKIE[$key] : '';
+function hook($hook_id, $args = array()) {
+    /** @var array $hooks */
+    $hooks = config($hook_id);
+    if ($hooks && is_array($args)) {
+        foreach ($hooks as $hook) {
+            call_user_func_array($hook, $args);
+            //$hook(...$args); // PHP 5.6+
+        }
+    } else if (is_callable($args)){ // save to hook
+        if (!$hooks) {
+            $hooks = array();
+        }
+        $hooks[] = $args;
+        config(array($hook_id => $hooks));
     }
 }
+
--- tests/Ling/commonTest.php
+++ tests/Ling/commonTest.php
@@ -11,7 +11,12 @@ class commonTest extends TestCase
         config(array('123' => '456'));
         $this->assertSame(config('123'), '456');
     }
-    
+
+    public function testEnvConfig() { // we need to add env test
+        putenv('LING_ENV=dev');
+        $this->assertSame(config('env.LING_ENV'), 'dev');
+    }
+
     public function testHook() {
         hook('hook.hook_id', function ($hello, $world) {
             $this->hook_result =  $hello . ', ' . $world;
